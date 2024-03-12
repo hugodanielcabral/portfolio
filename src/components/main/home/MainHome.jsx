4c2ea877d030f21870f17d9a2da83734002c7b9e
Daniel Cabral <danicabral15@outlook.com>
2024-03-11 23:18:14 -0300

    Add icons to resume and contact buttons

--- src/components/main/home/MainHome.jsx
+++ src/components/main/home/MainHome.jsx
@@ -1,6 +1,9 @@
+import { IoIosDocument } from "react-icons/io";
+import { MdOutlineMailOutline } from "react-icons/md";
+
 export const MainHome = () => {
   return (
-    <section id="home" className="justify-center w-3/4 mt-20 md:mt-10 lg:p-20">
+    <section id="home" className="justify-center w-3/4 mt-20 md:mt-8 lg:p-20">
       <h3 className="text-lg lg:text-2xl text-[#FF31AD]">Hello!, my name is</h3>
       <h1 className="my-3 text-4xl lg:text-6xl text-[#0E1F39] dark:text-[#FFF9F3] font-bold">
         Daniel Cabral.
@@ -14,14 +17,21 @@ export const MainHome = () => {
         technical expertise and innovation to dynamic projects.
       </h2>
       <article className="flex mt-3 gap-x-5">
-        <button className="btn btn-outline border-[#FF31AD] text-[#FF31AD]">
-          <a href="/resume_daniel_cabral_en.pdf" download>
-            Resume
-          </a>
-        </button>
-        <button className="btn btn-outline border-[#FF31AD] text-[#FF31AD]">
-          <a href="#contact">Contact</a>
-        </button>
+        <a
+          href="/resume_daniel_cabral_en.pdf"
+          download
+          className="btn bg-stone-700 hover:bg-stone-800 text-[#FF31AD] btn-wide text-lg dark:bg-slate-200 dark:hover:bg-slate-300"
+        >
+          Resume
+          <IoIosDocument />
+        </a>
+
+        <a
+          href="#contact"
+          className="btn bg-[#FF31AD] hover:bg-[#cf4e9b] text-white bg-opacity-75"
+        >
+          Contact <MdOutlineMailOutline />
+        </a>
       </article>
     </section>
   );
