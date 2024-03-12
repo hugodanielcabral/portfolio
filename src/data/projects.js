0098aeed858c379909f39d7f66c6113394487646
Daniel Cabral <danicabral15@outlook.com>
2024-03-12 13:09:23 -0300

    Add technologiesIcons.jsx and projects.js files

---
+++ src/data/projects.js
@@ -0,0 +1,71 @@
+import donc_image from "../images/donc_wallpaper.webp";
+import dog360_image from "../images/dog360_wallpaper.webp";
+import gamenest_image from "../images/gamenest_wallpaper.webp";
+
+export const MajorProjects = [
+  {
+    id: 1,
+    title: "GameNest (In progress)",
+    description:
+      "I am currently working on a website where you can create collection of games, find information about them and read reviews.",
+    image: gamenest_image,
+    technologies: ["React", "Node", "PostgreSQL", "Tailwind", "Jest", "Redux"],
+    repository: {
+      exist: false,
+      link: "#",
+    },
+    live: {
+      exist: false,
+      link: "#",
+    },
+    updates: {
+      exist: false,
+      link: "#",
+    },
+  },
+  {
+    id: 2,
+    title: "DonC Alojamientos",
+    description:
+      "I designed a website where you can book different types of houses and even publish your own.",
+    image: donc_image,
+    technologies: ["React", "Node", "PostgreSQL", "Tailwind"],
+    repository: {
+      exist: true,
+      link: "https://github.com/hugodanielcabral/cab_alojamientos",
+    },
+    live: {
+      exist: true,
+      link: "https://donc-alojamientos.onrender.com/",
+    },
+  },
+  {
+    id: 3,
+    title: "Dog360",
+    description:
+      "I worked on a website where you will find information about dogs, such as breed, size, weight, etc.",
+    image: dog360_image,
+    technologies: ["React", "Node", "MySQL", "Tailwind"],
+    repository: {
+      exist: true,
+      link: "https://github.com/hugodanielcabral/dog360",
+    },
+    live: {
+      exist: false,
+      link: "#",
+    },
+  },
+];
+
+export const minorProjects = [
+  {
+    id: 1,
+    title: "Dawson E-Commerce",
+    description: "I developed an E-Commerce following the SCRUM methodology.",
+    technologies: ["Javascript", "HTML5", "Bootstrap"],
+    repository: {
+      exist: true,
+      link: "https://github.com/dani-assa/Dawson-ecommerce",
+    },
+  },
+];
---
+++ src/data/technologiesIcons.jsx
@@ -0,0 +1,77 @@
+import { FaReact, FaHtml5 } from "react-icons/fa";
+import { DiNodejs } from "react-icons/di";
+import { BiLogoPostgresql } from "react-icons/bi";
+import {
+  SiTailwindcss,
+  SiMysql,
+  SiBootstrap,
+  SiJest,
+  SiRedux,
+  SiFirebase,
+} from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+
+const technologiesIcons = [
+  {
+    id: 1,
+    name: "React",
+    icon: <FaReact size={32} className="text-blue-400" />,
+  },
+  {
+    id: 2,
+    name: "Node",
+    icon: <DiNodejs size={60} className="text-green-400" />,
+  },
+  {
+    id: 3,
+    name: "PostgreSQL",
+    icon: <BiLogoPostgresql size={32} className="text-blue-400" />,
+  },
+  {
+    id: 4,
+    name: "Tailwind",
+    icon: <SiTailwindcss size={32} className="text-blue-400" />,
+  },
+  {
+    id: 5,
+    name: "MySQL",
+    icon: <SiMysql size={32} className="dark:text-amber-500 text-stone-800" />,
+  },
+  {
+    id: 6,
+    name: "Bootstrap",
+    icon: <SiBootstrap size={32} className="text-violet-500" />,
+  },
+  {
+    id: 7,
+    name: "Javascript",
+    icon: (
+      <IoLogoJavascript
+        size={32}
+        className="dark:text-[#F7DF1E] text-[#c0a705cd]"
+      />
+    ),
+  },
+  {
+    id: 8,
+    name: "HTML5",
+    icon: <FaHtml5 size={32} className="text-red-600" />,
+  },
+  {
+    id: 9,
+    name: "Jest",
+    icon: <SiJest size={32} className="text-red-600" />,
+  },
+  {
+    id: 10,
+    name: "Redux",
+    icon: <SiRedux size={32} className="text-purple-600" />,
+  },
+  {
+    id: 11,
+    name: "Firebase",
+    icon: <SiFirebase size={32} className="text-yellow-500" />,
+  },
+];
+
+export default technologiesIcons;
